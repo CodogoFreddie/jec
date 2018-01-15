c30817b5fcef18eeb59087f9469916b2cdf77749
Freddie Ridell <freddie@codogo.io>
2018-01-15 12:27:50 +0000

    feat(monorepo): lets try this for juicer managment of many interlinked packages

---
+++ README.md
@@ -0,0 +1,39 @@
+# JavascriptExoCortex
+
+An ecosystem of tools for storing generic stuctured data with various differnt viewing apps.
+
+<!-- vim-markdown-toc GFM -->
+* [jec-engine](#jec-engine)
+* [jec-config-fetcher](#jec-config-fetcher)
+* [jec-server](#jec-server)
+* [jec-cli-interface](#jec-cli-interface)
+* [jec-pure-cli](#jec-pure-cli)
+* [jec-client-cli](#jec-client-cli)
+* [jec-jask](#jec-jask)
+
+<!-- vim-markdown-toc -->
+
+## jec-engine
+The core logic and processing of the JEC system.
+This is a tool that accepts a stream of JEC compliant actions, and calculates the total state of a JEC instance. It also provides a querying language for reading data, and generating new actions.
+
+## jec-config-fetcher
+A package for reading the .jecrc.js config file that is used to configure all command line JEC tools
+
+## jec-server
+An HTTP server to wrap a running [`jec-engine`](#jec-engine) instance.
+
+## jec-cli-interface
+A package for provide an interface that a CLI querying provider must adhere to.
+A JEC enabled CLI program shouldn't have to handle the full [`jec-engine`](#jec-engine) itself, so this package provides an interface for accessing the state.
+There are two main implmentations, detailed below:
+
+## jec-pure-cli
+This CLI provider queries the filesystem for all actions, and reconstructs the full state on each run. This is simple, but ineficient
+
+## jec-client-cli
+This CLI provider queries a [`jec-server`](#jec-server) on each run. The server can stay active between queries, meaning that each query can be run much faster. This does however, require running a server.
+
+## jec-jask
+The first JEC enabled tool, which is a simple ToDo list manager.
+
--- jec-engine/src/index.js
+++ jec-engine/src/index.js
@@ -1,57 +1,57 @@
 import * as R from "ramda";
 
 import { insertAction, realiseActions, getState, } from "./chain";
-import { parseArgsList, } from "./parseArgsList";
-import * as opperations from "./opperations";
+//import { parseArgsList, } from "./parseArgsList";
+//import * as opperations from "./opperations";
 
-let listActionsHandler = () => {};
-let readActionHandler = () => {};
-let writeActionHandler = () => {};
+//let listActionsHandler = () => {};
+//let readActionHandler = () => {};
+//let writeActionHandler = () => {};
 
-export const setPersistHandlers = ({
-	listActions,
-	readAction,
-	writeAction,
-}) => {
-	listActionsHandler = listActions;
-	readActionHandler = R.memoizeWith(R.identity, readAction);
-	writeActionHandler = writeAction;
-};
+//export const setPersistHandlers = ({
+	//listActions,
+	//readAction,
+	//writeAction,
+//}) => {
+	//listActionsHandler = listActions;
+	//readActionHandler = R.memoizeWith(R.identity, readAction);
+	//writeActionHandler = writeAction;
+//};
 
-export const initaliseJask = () =>
-	listActionsHandler()
-		.then(actions => Promise.all(actions.map(readActionHandler)))
-		.then(R.sortBy(R.path(["meta", "timestamp",])))
-		.then(realiseActions);
+//export const initaliseJask = () =>
+	//listActionsHandler()
+		//.then(actions => Promise.all(actions.map(readActionHandler)))
+		//.then(R.sortBy(R.path(["meta", "timestamp",])))
+		//.then(realiseActions);
 
-export const runQuery = query => {
-	const {
-		filter,
-		keyword,
-		modifiers,
-		filterPresent,
-		modifiersPresent,
-	} = parseArgsList(query.split(" "));
+//export const runQuery = query => {
+	//const {
+		//filter,
+		//keyword,
+		//modifiers,
+		//filterPresent,
+		//modifiersPresent,
+	//} = parseArgsList(query.split(" "));
 
-	const {
-		opperationFilter,
-		opperationActionCreator,
-		returnFilter,
-	} = (opperations[keyword] || opperations.noop)({
-		filter,
-		filterPresent,
-		modifiers,
-		modifiersPresent,
-	});
+	//const {
+		//opperationFilter,
+		//opperationActionCreator,
+		//returnFilter,
+	//} = (opperations[keyword] || opperations.noop)({
+		//filter,
+		//filterPresent,
+		//modifiers,
+		//modifiersPresent,
+	//});
 
-	const newActions = opperationActionCreator(
-		getState().filter(opperationFilter),
-	);
+	//const newActions = opperationActionCreator(
+		//getState().filter(opperationFilter),
+	//);
 
-	newActions.forEach(writeActionHandler);
+	//newActions.forEach(writeActionHandler);
 
-	newActions.forEach(insertAction);
+	//newActions.forEach(insertAction);
 
-	return getState().filter(returnFilter);
-};
+	//return getState().filter(returnFilter);
+//};
 
