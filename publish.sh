b04b770f27ad3b84faf59bf23e1b445ff661a170
Freddie Ridell <freddie@codogo.io>
2018-01-16 14:35:32 +0000

    feat(publishing-script): added a quick publishing script to publish things in tandam

--- publish.sh
+++ publish.sh
@@ -9,19 +9,20 @@ for package in $( jq ".workspaces[]" -cr package.json ) ; do
 		yarn upgrade-interactive --latest
 		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-		#yarn format
-		#rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-		#yarn build
-		#rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-		echo "yarn version --no-git-tag-version --new-version $1"
+		yarn format
 		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-		echo "yarn publish"
+		yarn build
+		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+		yarn version --no-git-tag-version --new-version $1
+		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+		yarn publish
 		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 	popd
 
+	yarn upgrade --latest
 done
 
 git commit -am "release($1)"
