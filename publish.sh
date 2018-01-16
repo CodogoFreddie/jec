f01c20e9a53844afed93b5f7d5e20b6f59a876a2
Freddie Ridell <freddie@codogo.io>
2018-01-16 14:34:51 +0000

    release()

--- publish.sh
+++ publish.sh
@@ -6,12 +6,14 @@ for package in $( jq ".workspaces[]" -cr package.json ) ; do
 		echo
 		echo
 		echo "Publishing $package"
-
-		yarn format
+		yarn upgrade-interactive --latest
 		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-		yarn build
-		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+		#yarn format
+		#rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+		#yarn build
+		#rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 		echo "yarn version --no-git-tag-version --new-version $1"
 		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
@@ -20,7 +22,6 @@ for package in $( jq ".workspaces[]" -cr package.json ) ; do
 		rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 	popd
 
-	yarn upgrade --latest
 done
 
 git commit -am "release($1)"
