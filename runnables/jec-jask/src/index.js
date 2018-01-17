53bd0131ca3102d30e6bc00115c172f5e26e8ed2
Freddie Ridell <freddie@codogo.io>
2018-01-17 15:13:23 +0000

    feat(middleware): started work on making an auto due-handler

--- runnables/jec-jask/src/index.js
+++ runnables/jec-jask/src/index.js
@@ -1,3 +1,7 @@
+import * as R from "ramda";
+import genUUID from "uuid/v4";
+import produceUUID from "uuid/v3";
+
 import startPure from "jec-pure-cli";
 
 const createConfigIfNeeded = opperators => {
@@ -29,9 +33,61 @@ const createConfigIfNeeded = opperators => {
 	}
 };
 
-startPure(console.log)
-	.then(createConfigIfNeeded)
-	.then(({ getState, getConfig, }) => {
-		console.log(getState());
-		console.log(getConfig());
-	});
+//startPure(console.log)
+	//.then(createConfigIfNeeded)
+	//.then(({ getState, getConfig, }) => {
+		//console.log(getState());
+		//console.log(getConfig());
+//});
+
+const hashToUUID = x => produceUUID(x, "1b671a64-40d5-491e-99b0-da01ff1f3341");
+
+const input = { 
+	action: {
+		meta: {
+			time: 123,
+			action: "actionid",
+			obj: "objid",
+		},
+		mutations: [
+			{
+				type: "assoc",
+				path: [ "done", ],
+				value: 123,
+			}
+		],
+	},
+	obj: {
+		uuid: "objid",
+		description: "this is the description",
+		tags: [
+			"tag1", "tag2",
+		],
+	},
+};
+
+const derivedActions = R.when(
+	R.path([ "obj", "description", ]),
+	R.pipe(
+		R.over(
+			R.lens(
+				R.path(["obj", "uuid",]),
+				R.assocPath([ "newAction", "meta", "obj", ]),
+			),
+			hashToUUID
+		),
+
+		R.prop("newAction"),
+	),
+)(input);
+
+console.log(
+	JSON.stringify(
+		{
+			input,
+			derivedActions,
+		},
+		null,
+		2
+	)
+);
