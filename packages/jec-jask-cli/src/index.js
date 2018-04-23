931198d2f5498538944bf95274cf9f4baaa19fc8
Freddie Ridell <freddie@codogo.io>
2018-04-23 14:02:51 +0100

    lint

--- packages/jec-jask-cli/src/index.js
+++ packages/jec-jask-cli/src/index.js
@@ -1,82 +1,82 @@
-import createJecJaskStore, {collateAllObjects} from 'jec-jask';
-import * as R from 'ramda';
+import createJecJaskStore, { collateAllObjects, } from "jec-jask";
+import * as R from "ramda";
 
-import parseCli from './parseCli';
-import generateAddActions from './generateAddActions';
-import generateModifyActions from './generateModifyActions';
-import generateWorkflowEventActions from './generateWorkflowEventActions';
-import filterByCLICommands from './filterByCLICommands';
-import render from './render';
+import parseCli from "./parseCli";
+import generateAddActions from "./generateAddActions";
+import generateModifyActions from "./generateModifyActions";
+import generateWorkflowEventActions from "./generateWorkflowEventActions";
+import filterByCLICommands from "./filterByCLICommands";
+import render from "./render";
 
 import {
-  listAllActions,
-  writeAction,
-  readAction,
-  persistStorage,
-} from './distributeHooks';
+	listAllActions,
+	writeAction,
+	readAction,
+	persistStorage,
+} from "./distributeHooks";
 
-process.on('unhandledRejection', r => console.log(r));
+process.on("unhandledRejection", r => console.log(r));
 
 const store = createJecJaskStore({
-  listAllActions,
-  writeAction,
-  readAction,
-  persistStorage,
+	listAllActions,
+	writeAction,
+	readAction,
+	persistStorage,
 });
 
 let hasRunCliCommand = false;
 store.subscribe(() => {
-  if (!hasRunCliCommand) {
-    const {__distributeStatus} = store.getState();
+	if (!hasRunCliCommand) {
+		const { __distributeStatus, } = store.getState();
 
-    if (__distributeStatus === 'READY') {
-      hasRunCliCommand = true;
+		if (__distributeStatus === "READY") {
+			hasRunCliCommand = true;
 
-      const {filter, command, modifications} = parseCli(process.argv);
+			const { filter, command, modifications, } = parseCli(process.argv);
 
-      const noop = () => ({
-        actions: [],
-        filterForRender: filterByCLICommands(filter),
-      });
+			const noop = () => ({
+				actions: [],
+				filterForRender: filterByCLICommands(filter),
+			});
 
-      const actionGenerator =
-        {
-          add: generateAddActions,
-          modify: generateModifyActions,
-          start: generateWorkflowEventActions('start'),
-          stop: generateWorkflowEventActions('stop'),
-          done: generateWorkflowEventActions('done'),
-        }[command] || noop;
+			const actionGenerator =
+				{
+					add: generateAddActions,
+					modify: generateModifyActions,
+					start: generateWorkflowEventActions("start"),
+					stop: generateWorkflowEventActions("stop"),
+					done: generateWorkflowEventActions("done"),
+				}[command] || noop;
 
-      const {actions, filterForRender} = actionGenerator({
-        filter,
-        modifications,
-        state: store.getState(),
-      });
+			const { actions, filterForRender, } = actionGenerator({
+				filter,
+				modifications,
+				state: store.getState(),
+			});
 
-      actions.forEach(store.dispatch);
+			actions.forEach(store.dispatch);
 
-      console.log(
-        render(
-          {
-            filterTask: R.identity,
-            giveColor: () => [],
-            headers: [
-              'id',
-              'score',
-              'description',
-              'priority',
-              'tags',
-              'project',
-              'due',
-              'start',
-              'done',
-              'recur',
-            ],
-          },
-          collateAllObjects(store.getState()).filter(filterForRender),
-        ),
-      );
-    }
-  }
+			console.log(
+				render(
+					{
+						filterTask: R.identity,
+						giveColor: () => [],
+						headers: [
+							"id",
+							"score",
+							"description",
+							"priority",
+							"tags",
+							"project",
+							"due",
+							"start",
+							"done",
+							"recur",
+						],
+					},
+					collateAllObjects(store.getState()).filter(filterForRender),
+				),
+			);
+		}
+	}
 });
--- packages/jec-jask/src/giveScore.js
+++ packages/jec-jask/src/giveScore.js
@@ -1,24 +1,27 @@
-import * as R from 'ramda';
+import * as R from "ramda";
 
 const giveScore = R.pipe(
-  R.converge(
-    (...scores) =>
-      R.pipe(R.filter(Boolean), R.filter(x => typeof x === 'number'), R.sum)(
-        scores,
-      ),
-    [
-      ({due}) => (new Date().getTime() - new Date(due).getTime()) / 86400000,
-      ({tags}) => tags.length,
-      ({project}) => (project ? 3 : 0),
-      ({start}) => (start ? 5 : 0),
-      ({priority}) =>
-        ({
-          H: 10,
-          M: 5,
-          L: 2,
-        }[priority]),
-    ],
-  ),
+	R.converge(
+		(...scores) =>
+			R.pipe(
+				R.filter(Boolean),
+				R.filter(x => typeof x === "number"),
+				R.sum,
+			)(scores),
+		[
+			({ due, }) =>
+				(new Date().getTime() - new Date(due).getTime()) / 86400000,
+			({ tags, }) => tags.length,
+			({ project, }) => (project ? 3 : 0),
+			({ start, }) => (start ? 5 : 0),
+			({ priority, }) =>
+				({
+					H: 10,
+					M: 5,
+					L: 2,
+				}[priority]),
+		],
+	),
 );
 
 export default giveScore;
